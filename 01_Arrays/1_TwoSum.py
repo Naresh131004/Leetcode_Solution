d843ccbd5b5eceaebbae7d32e9e438777461e608
Naresh131004 <naresh131004@gmail.com>
2026-07-28 19:41:21 +0530

    Leetcode Array problem Solutions

---
+++ 01_Arrays/1_TwoSum.py
@@ -0,0 +1,10 @@
+Class Solution:
+	def TwoSum(self,nums:List[int],target:int)->List[int]:
+		seen = {}
+		
+		for index,num in enumerate(nums):
+			complement = target - num
+			
+			if complement in seen:
+				return[seen[complement],index]
+			seen[num] = index
